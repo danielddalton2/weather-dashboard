4a0db0f576e5939c12eb400b2d062556546a3dc2
Dan Dalton <116042742+danielddalton2@users.noreply.github.com>
2022-11-29 09:07:59 -0800

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var apiKey = "1f0e71fffb79c10df606a7ce0db76164";
+var searchInput = document.getElementById("search");
+var searchForm = document.querySelector("#searchForm");
+
+function getWeatherData(event) {
+  event.preventDefault();
+  var cityName = searchInput.value;
+  console.log(cityName);
+  var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+searchForm.addEventListener("submit", getWeatherData);
